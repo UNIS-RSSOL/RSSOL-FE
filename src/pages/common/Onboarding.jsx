0b6a86e8c953324cc390b36784477d56da43880b
zieyou52 <zieyou52@ewha.ac.kr>
2025-11-09 20:09:24 +0900

    스플래쉬 변경

---
+++ src/pages/common/Onboarding.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function OwnerPage() {
+  return (
+    <div className="w-full h-full bg-[#F8FBFE]">
+      <p>Onboarding page</p>
+    </div>
+  );
+}
+
+export default OwnerPage;
