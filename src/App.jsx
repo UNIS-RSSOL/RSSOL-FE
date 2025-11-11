a24807e79950f5323b4603e9c390b8bccbc34ac4
zieyou52 <zieyou52@ewha.ac.kr>
2025-11-11 11:15:28 +0900

    온보딩 파일 위치 변경 및 App.jsx main.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import "./App.css";
 import "./index.css";
 import Header from "./components/layout/header/Header.jsx";
 import Footer from "./components/layout/footer/Footer.jsx";
+
+import Login from "./pages/auth/Login.jsx";
+import Onboarding from "./pages/auth/Onboarding.jsx";
+
 import OwnerPage from "./pages/owner/mypage/OwnerPage.jsx";
 import ManageStore from "./pages/owner/mypage/ManageStore.jsx";
 import OwnerCalendar from "./pages/owner/calendar/OwnerCalendar.jsx";
@@ -14,11 +18,12 @@ function App() {
       <Header />
       <main className="flex-1 overflow-y-auto">
         <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/onboarding" element={<Onboarding />} />
           <Route path="/ownerpage" element={<OwnerPage />} />
           <Route path="/ownerpage/managestore" element={<ManageStore />} />
           <Route path="/employeepage" element={<EmployeePage />} />
           <Route path="/employeepage/managestore" element={<ManageStore />} />
-
           <Route path="/ownercalendar" element={<OwnerCalendar />} />
         </Routes>
       </main>
--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 const Root = () => {
   const [showSplash, setShowSplash] = useState(true);
-
+  
   useEffect(() => {
-    const timer = setTimeout(() => setShowSplash(false), 2000);
+    const timer = setTimeout(() => setShowSplash(false), 3000);
     return () => clearTimeout(timer);
   }, []);
 
