262a71a880343fcf765c9215d6aa4b2e3df2c3c6
박수진 <sherry5667@naver.com>
2025-11-18 01:41:59 +0900

    - Style: 관리 페이지 UI
    - 사장님 직원관리 페이지 완료
    - 알바생 급여관리 페이지 완료

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="w-full h-[42px] bg-red-500 flex items-center">
-      <p className="text-white font-bold">알쏠</p>
+    <div className="w-full h-[42px] bg-[#fdfffe] flex items-center">
+      <p className="text-black font-bold">알쏠</p>
     </div>
   );
 }
