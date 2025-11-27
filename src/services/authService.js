fced087a863fb2d6a7bdc059103c1c3a6e8900c9
박수진 <sherry5667@naver.com>
2025-11-27 18:49:29 +0900

    주소 정상화

--- src/services/authService.js
+++ src/services/authService.js
@@ -8,7 +8,7 @@ import api from "./api"; // 수정된 Axios 인스턴스 가져오기
 export const getDevToken = async (email) => {
   try {
     const response = await api.post(
-      "/api/auth/dev-token",
+      "/auth/dev-token",
       JSON.stringify({ email }),
       {
         headers: {
