7067bbfb242c6cd9f28f29b69381c9c00f36a690
박수진 <sherry5667@naver.com>
2025-11-27 18:24:18 +0900

    - api.js에 authorization 추가

--- src/services/api.js
+++ src/services/api.js
@@ -7,6 +7,7 @@ const getAuthToken = () => {
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
+    Authorization: `Bearer ${getAuthToken}`,
     "Content-Type": "application/json",
   },
   withCredentials: true,
