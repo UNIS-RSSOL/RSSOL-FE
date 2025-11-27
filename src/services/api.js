46f880d4b31d6136e30dba12561984e6a9981cea
박수진 <sherry5667@naver.com>
2025-11-27 17:46:09 +0900

    api헤더

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNzYzNTY0NzY2LCJleHAiOjE3NjM1NjgzNjZ9.8Pk4hXTzlppR49K6_RHwQo8QTdRrdngUMCIk76-XYro",
-
     "Content-Type": "application/json",
   },
   withCredentials: false,
