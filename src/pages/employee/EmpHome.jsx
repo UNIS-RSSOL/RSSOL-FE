eb27b57b357aabce31c00f9a11e10d8bf43d24d4
박수진 <sherry5667@naver.com>
2025-12-02 21:36:23 +0900

    - 직원 활성매장 변경로직 추가

--- src/pages/employee/EmpHome.jsx
+++ src/pages/employee/EmpHome.jsx
@@ -14,6 +14,7 @@ import {
   fetchActiveStore,
   fetchMydata,
   fetchStoreList,
+  changeActiveStore,
 } from "../../services/employee/MyPageService.js";
 import dayjs from "dayjs";
 import { motion, AnimatePresence } from "framer-motion";
