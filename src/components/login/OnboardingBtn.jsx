b54d54cdaf745d273def627b218a9927d40bfe3b
zieyou52 <zieyou52@ewha.ac.kr>
2026-03-14 21:43:21 +0900

    전체 너비 수정

--- src/components/login/OnboardingBtn.jsx
+++ src/components/login/OnboardingBtn.jsx
@@ -12,7 +12,7 @@ function OnboardingBtn({ onClick, children, role, value }) {
   return (
     <Button
       onClick={handleSelect}
-      className={`relative h-[123px] w-[360px] !justify-start pl-[35px] rounded-[11px] ${selected ? "!bg-[#CCDBFF] !border-[1.09px] !border-[#3370FF]" : "!bg-[#EDF0F7] border-[1.09px] border-transparent shadow-[0_4px_8px_0_rgba(0,0,0,0.2)]"}`}
+      className={`relative h-[123px] w-full max-w-[360px] !justify-start pl-[35px] rounded-[11px] ${selected ? "!bg-[#CCDBFF] !border-[1.09px] !border-[#3370FF]" : "!bg-[#EDF0F7] border-[1.09px] border-transparent shadow-[0_4px_8px_0_rgba(0,0,0,0.2)]"}`}
     >
       {children}
     </Button>
