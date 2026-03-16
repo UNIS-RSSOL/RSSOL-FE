8f03e6433f3ea55bf53d9b89020b361391850669
박수진 <sherry5667@naver.com>
2026-03-16 14:14:28 +0100

    사이드바 연결

--- src/pages/owner/manage/AllStaff.jsx
+++ src/pages/owner/manage/AllStaff.jsx
@@ -24,44 +24,6 @@ function AllStaff() {
 
   return (
     <div className="w-full">
-      <div className="flex w-full bg-[#FDFFFE]">
-        <div
-          className="w-1/2 relative"
-          onClick={() => {
-            setTab(0);
-            navigate("/owner/manage/employee");
-          }}
-        >
-          <div
-            className={`text-[18px] font-[500] cursor-pointer ${tab === 0 ? "text-black" : "text-[#87888C]"}`}
-          >
-            <div className="w-[80%] mx-auto font-[500] text-[18px]">
-              전체 직원
-              {tab === 0 && (
-                <div className="w-full h-[3px] bg-[#6694FF] rounded-full"></div>
-              )}
-            </div>
-          </div>
-        </div>
-        <div
-          className="w-1/2 relative"
-          onClick={() => {
-            setTab(1);
-            navigate("/owner/manage/wage");
-          }}
-        >
-          <div
-            className={`text-[18px] font-[500] cursor-pointer ${tab === 1 ? "text-black" : "text-[#87888C]"}`}
-          >
-            <div className="w-[80%] mx-auto font-[500] text-[18px]">
-              급여 관리
-              {tab === 1 && (
-                <div className="w-full h-[3px] bg-[#6694FF] rounded-full"></div>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
       <div className="flex flex-col w-full h-full items-center mt-5 gap-5 ">
         {employees?.map(
           (emp) =>
