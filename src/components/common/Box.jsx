3c725b6cde7642ca9864ec38812781136cf279ed
Hee Seo Kim <hisounni@ewhain.net>
2026-03-21 21:14:24 +0900

    그림자 제거

--- src/components/common/Box.jsx
+++ src/components/common/Box.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function Box({ disabled, children, className = "", onClick }) {
   return (
     <div
-      className={`flex w-full p-[15px] bg-white rounded-[15px] shadow-[0px_4px_8px_0px_rgba(0,0,0,0.20)] items-center bg-[#FDFFFE] ${disabled ? "cursor-default" : "cursor-pointer"} ${className}`}
+      className={`flex w-full p-[15px] bg-white rounded-[15px] border border-[#e7eaf3] items-center bg-[#FDFFFE] ${disabled ? "cursor-default" : "cursor-pointer"} ${className}`}
       onClick={onClick}
     >
       {children}
