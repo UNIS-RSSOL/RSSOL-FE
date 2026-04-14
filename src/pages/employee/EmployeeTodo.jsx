9cc5ed37726666ab5449fb95fc6c220f9ffb37eb
zieyou52 <zieyou52@ewha.ac.kr>
2026-04-14 21:17:11 +0900

    employeetodo footer 수정

--- src/pages/employee/EmployeeTodo.jsx
+++ src/pages/employee/EmployeeTodo.jsx
@@ -16,14 +16,10 @@ import {
 } from "../../services/TodoService.js";
 
 import BellIcon from "../../assets/icons/BellIcon.jsx";
-import CalendarIcon from "../../assets/icons/CalendarIcon.jsx";
-import EditIcon from "../../assets/icons/EditIcon.jsx";
-import FileEditIcon from "../../assets/icons/FileEditIcon.jsx";
 import TodoAllIcon from "../../assets/icons/TodoAllIcon.jsx";
 import TodoPartnerIcon from "../../assets/icons/TodoPartnerIcon.jsx";
 import TodoUserIcon from "../../assets/icons/TodoUserIcon.jsx";
-import TodoSelectIcon from "../../assets/icons/TodoSelectIcon.jsx";
-import FooterMenu from "../../components/layout/footer/FooterMenu.jsx";
+import Footer from "../../components/layout/footer/Footer.jsx";
 
 import HomeHeader from "../../components/home/HomeHeader.jsx";
 import HomeSidebar from "../../components/home/HomeSidebar.jsx";
@@ -559,42 +555,7 @@ function EmployeeTodo() {
         </div>
       </main>
 
-      <nav className="w-full h-[60px] flex flex-row justify-around items-center shrink-0 shadow-[0_-2px_7px_0_rgba(0,0,0,0.1)] bg-white">
-        <FooterMenu
-          MenuIcon={
-            <svg width="20" height="20" viewBox="0 0 20 20" fill="none">
-              <path
-                d="M1 7.41605C1 7.04666 1.18802 6.7001 1.50457 6.48603L9.30457 1.21117C9.72092 0.929609 10.2791 0.92961 10.6954 1.21117L18.4954 6.48603C18.812 6.7001 19 7.04665 19 7.41605V17.2882C19 18.2336 18.1941 19 17.2 19H2.8C1.80589 19 1 18.2336 1 17.2882V7.41605Z"
-                fill="none"
-              />
-              <path
-                d="M5.5 15.0625H14.5M9.30457 1.21117L1.50457 6.48603C1.18802 6.7001 1 7.04666 1 7.41605V17.2882C1 18.2336 1.80589 19 2.8 19H17.2C18.1941 19 19 18.2336 19 17.2882V7.41605C19 7.04665 18.812 6.7001 18.4954 6.48603L10.6954 1.21117C10.2791 0.92961 9.72092 0.929609 9.30457 1.21117Z"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          }
-          title="홈"
-          onClick={() => navigate("/employee")}
-        />
-        <FooterMenu
-          MenuIcon={<CalendarIcon />}
-          title="캘린더"
-          onClick={() => navigate("/employee/calendar")}
-        />
-        <FooterMenu
-          MenuIcon={<TodoSelectIcon />}
-          title="할 일"
-          onClick={() => {}}
-        />
-        <FooterMenu
-          MenuIcon={<FileEditIcon />}
-          title="근무표 제출"
-          onClick={() => navigate("/employee/schedule/modifying")}
-        />
-      </nav>
+      <Footer />
       <HomeSidebar
         isOpen={sidebarOpen}
         onClose={() => setSidebarOpen(false)}
